6ff9ace51a719ca776f880b1f78d66702aa1b382
Gonzalez32 <3232rose@gmail.com>
2021-02-05 12:49:21 -0600

    modified templates/Time & Date is rendering on page

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from datetime import date
+from datetime import datetime, date, time
 from django.contrib.auth.models import User
 
 
@@ -10,6 +10,8 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
+    post_time = models.TimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
