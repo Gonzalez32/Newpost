5fa6d7713ab1a267b93b3e03f513fd690e2515de
Gonzalez32 <3232rose@gmail.com>
2021-02-05 16:16:34 -0600

    added new model category for post

--- main_app/models.py
+++ main_app/models.py
@@ -6,12 +6,23 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
     post_time = models.TimeField(auto_now_add=True)
+    category = models.CharField(max_length=255, default='coding')
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
