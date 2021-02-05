6ac6fcca4a455d087d7d2c9514c553939f40d175
Gonzalez32 <3232rose@gmail.com>
2021-02-05 16:54:39 -0600

    added category to render on page

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,15 +1,23 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
 
+
+choices = Category.objects.all().values_list('name', 'name')
+
+choices_list = []
+
+for item in choices:
+    choices_list.append(item)
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'author', 'body')
+        fields = ('title', 'author', 'category', 'body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'input is-warning', 'placeholder': 'Enter Title...'}),
             'author': forms.Select(attrs={'class': 'select is-warning'}),
+            'category': forms.Select(choices=choices_list, attrs={'class': 'select is-warning'}),
             'body': forms.Textarea(attrs={'class': 'textarea is-warning', 'placeholder': 'Enter Post...'}),
         }
 
