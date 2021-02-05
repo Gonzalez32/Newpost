072f4e2f6c81dbd8672060346bc7bb27d6fea711
Gonzalez32 <3232rose@gmail.com>
2021-02-04 22:14:33 -0600

    added formfor post

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, PostDetailView
+from .views import HomeView, PostDetailView, AddPostView
 
 
 urlpatterns = [
     # path('', views.home, name='home'),
     path('', HomeView.as_view(), name='home'),
-    path('article/<int:pk>', PostDetailView.as_view(), name='article-detail')
+    path('article/<int:pk>', PostDetailView.as_view(), name='article-detail'),
+    path('add_post/', AddPostView.as_view(), name='add_post')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
@@ -16,3 +16,7 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
 
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
