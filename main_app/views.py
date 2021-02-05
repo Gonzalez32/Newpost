1cd0f23c7ef715cc2085e47e25e655e1fd0cbe07
Gonzalez32 <3232rose@gmail.com>
2021-02-05 11:37:38 -0600

    modified main_app/render delete_post

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditPostForm
+from django.urls import reverse_lazy
 
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-id']
 
 class PostDetailView(DetailView):
     model = Post
@@ -23,3 +25,8 @@ class UpdatePostView(UpdateView):
     form_class = EditPostForm
     template_name = 'update_post.html'
     # fields = ['title', 'body']
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
