ccb1e9f8dd5251cf31dc2e3a6c177af9764a0559
Gonzalez32 <3232rose@gmail.com>
2021-02-10 02:06:43 -0600

    render profile style

--- members/views.py
+++ members/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from django.views.generic import DetailView, CreateView
+from django.views.generic import DetailView, CreateView, UpdateView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
