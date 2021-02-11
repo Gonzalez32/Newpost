125a88b4b795db35ed87750d10786a3b9ff6af0c
Gonzalez32 <3232rose@gmail.com>
2021-02-11 11:00:33 -0600

    newupdate

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,13 +1,13 @@
 from django import forms
-from .models import Post, Comment
+from .models import Post, Comment, Category
 
 
-# choices = Category.objects.all().values_list('name', 'name')
+choices = Category.objects.all().values_list('name', 'name')
 
-# choices_list = []
+choices_list = []
 
-# for item in choices:
-#     choices_list.append(item)
+for item in choices:
+    choices_list.append(item)
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -17,7 +17,7 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'input is-warning', 'placeholder': 'Enter Title...'}), 
             'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'elder', 'type':'hidden'}),
-            # 'category': forms.Select(choices=choices_list, attrs={'class': 'select is-warning'}),
+            'category': forms.Select(choices=choices_list, attrs={'class': 'select is-warning'}),
             'body': forms.Textarea(attrs={'class': 'textarea is-warning', 'placeholder': 'Enter Post...'}),
             'snippet': forms.Textarea(attrs={'class': 'textarea is-warning', 'placeholder': 'Enter Snippet...'}),
         }
