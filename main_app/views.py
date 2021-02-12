7160dd248ff189a5d90adcb0b08692a14545f158
Gonzalez32 <3232rose@gmail.com>
2021-02-12 14:08:33 -0600

    modified main_app for bugs

--- main_app/views.py
+++ main_app/views.py
@@ -23,8 +23,8 @@ class HomeView(ListView):
     model = Post
     template_name = 'home.html'
     cats = Category.objects.all()
-    # ordering = ['-id']
-    ordering = ['-post_date']
+    ordering = ['-id']
+    # ordering = ['-post_date']
 
     def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
